9d681cd4320ce140223b0a06804064407ca80dac
Paul Holden <pholden@stria.com>
2017-12-15 11:22:36 -0800

    Make token generation generic

--- tool/gentoken.js
+++ tool/gentoken.js
@@ -1,11 +1,45 @@
-const reports = require('../lib/reports/token');
+const async = require('async');
+const token = require('../lib/token');
 
 require('dotenv').config();
 
-reports(process.env.REPORTS_EMAIL_ID, process.env.REPORTS_PASSWORD, (err, token) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('Your Zoho Reports auth token: ' + token);
+const apps = [
+	{
+		email_id: process.env.REPORTS_EMAIL_ID,
+		password: process.env.REPORTS_PASSWORD,
+		scope: 'ZohoReports/reportsapi'
+	},
+	{
+		email_id: process.env.SUPPORT_EMAIL_ID,
+		password: process.env.SUPPORT_PASSWORD,
+		scope: 'ZohoSupport/supportapi'
 	}
+];
+
+async.each(apps, (item, callback) => {
+	if (!item.scope) {
+		console.log('Invalid scope');
+		return callback();
+	}
+
+	if (!item.email_id) {
+		console.log(`Missing EMAIL_ID for scope ${item.scope}`)
+		return callback();
+	}
+
+	if (!item.password) {
+		console.log(`Missing PASSWORD for scope ${item.scope}`);
+		return callback();
+	}
+
+	token(item.email_id, item.password, item.scope, (err, token) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(`Your ${item.scope} auth token: ${token}`);
+		}
+
+		callback();
+	});
+}, (err) => {
 });
