e06469319b62f5dcae3f8ff655d8936d253e714a
Paul Holden <pholden@stria.com>
2017-12-15 10:33:58 -0800

    Cut down on test code
    - Will create a more robust test suite later

--- test.js
+++ test.js
@@ -12,75 +12,14 @@ async.waterfall([
 		});
 	},
 	(reports, callback) => {
-		reports.database('Test', (err, db) => {
+		reports.databases((err, dbnames) => {
 			if (err) {
 				return callback(err);
 			}
 
-			callback(null, db);
-		});
-	},
-	(db, callback) => {
-		db.table('SpringCM Backup History', (err, t) => {
-			if (err) {
-				return callback(err);
-			}
-
-			callback(null, t);
-		});
-	},
-	(t, callback) => {
-		t.addrow({
-			'Backup Date': '12/14/2017',
-			'Account Name': 'Paul Holden UAT',
-			'Account ID': '12345'
-		}, (err, row) => {
-			if (err) {
-				return callback(err);
-			}
-
-			console.log(row);
+			console.log(dbnames);
 			callback();
-		})
-	}
-], (err) => {
-	if (err) {
-		console.log(err);
-	}
-});
-
-async.waterfall([
-	(callback) => {
-		zoho.reports((err, reports) => {
-			if (err) {
-				return callback(err);
-			}
-
-			callback(null, reports);
 		});
-	},
-	(reports, callback) => {
-		reports.table('Test', 'SpringCM Backup History', (err, t) => {
-			if (err) {
-				return callback(err);
-			}
-
-			callback(null, t);
-		});
-	},
-	(t, callback) => {
-		t.addrow({
-			'Backup Date': '12/14/2017',
-			'Account Name': 'Paul Holden UAT',
-			'Account ID': '12345'
-		}, (err, row) => {
-			if (err) {
-				return callback(err);
-			}
-
-			console.log(row);
-			callback();
-		})
 	}
 ], (err) => {
 	if (err) {
